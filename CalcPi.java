3fec916c1e0a7196c2a56ba6ca7b70bf257810c7
Ran Shaked <ran.shaked@runi.ac.il>
2024-11-17 11:11:04 +0200

    new

--- CalcPi.java
+++ CalcPi.java
@@ -5,19 +5,17 @@ public class CalcPi {
 		double numOperations = Integer.parseInt(args[0]);
 	    double pi = Math.PI;
 		System.out.println("pi according to Java: " +pi);
-		double piMe = 0.0;
-		double i = 0.0;
-		while(numOperations>0)
+		double piMe = 1.0;
+		for(int i = 2 ; i <= numOperations ; i++)
 		{
-			 i = 1/((2*numOperations)-1);
-			if(numOperations%2 == 0)
+			double num1 = (i*2)-1;
+			if(i%2 == 0)
 			{
-				piMe = piMe - i;
+				piMe = piMe - (1/num1);
 			}else{
-				piMe = piMe + i; 
+				piMe = piMe + (1/num1);
 			}
-			numOperations--;
 		}
 		System.out.println("pi, approximated:     " + piMe*4);
 	}
-}
+}//
